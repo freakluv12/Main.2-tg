7b930c27764eb2a9ccf61b3fe64b83aef8113e62
freakluv12 <130093288+freakluv12@users.noreply.github.com>
2025-09-02 10:34:59 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 # Telegram Bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = os.getenv("ADMIN_ID", "@default_user")  # Используй строку по умолчанию, если переменная не задана
 
 # Database
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/rental_crm")
