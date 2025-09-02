5d8805ab07f37c694152430f3cb4f5cf3e0a4d1b
freakluv12 <130093288+freakluv12@users.noreply.github.com>
2025-09-02 10:42:01 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID", "@default_user")  # Используй строку по умолчанию, если переменная не задана
 
 # Database
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/rental_crm")
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 # Web Interface
 SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key-change-this")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
